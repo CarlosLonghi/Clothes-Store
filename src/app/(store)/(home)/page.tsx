fb4f41cb68a0efb573706096e6f70c982734a0ae
CarlosLonghi <carloslonghi.cl@gmail.com>
2024-05-23 00:16:11 -0300

    feat: add page home

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,5 +1,72 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default async function Home() {
   await new Promise((resolve) => setTimeout(resolve, 2000))
 
-  return <h1>Hi :)</h1>
+  return (
+    <div className="grid grid-cols-9 grid-rows-6 gap-4">
+      <Link
+        href={'#'}
+        className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-center"
+      >
+        <Image
+          className="scale-95 group-hover:scale-100 transition-transform duration-200"
+          src={'/images/shirt-1.png'}
+          width={550}
+          height={550}
+          quality={100}
+          alt=""
+        />
+
+        <div className="absolute bottom-16 right-8 h-12 flex items-center gap-2 max-w-64 rounded-full border-2 border-zinc-500 bg-black/50 p-1 pl-5">
+          <span className="text-sm truncate">Hoop Culture Shirt</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-indigo-600 px-4 font-semibold whitespace-nowrap">
+            R$ 150,00
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        href={'#'}
+        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-center"
+      >
+        <Image
+          className="scale-95 group-hover:scale-100 transition-transform duration-200"
+          src={'/images/shirt-2.png'}
+          width={300}
+          height={300}
+          quality={100}
+          alt=""
+        />
+
+        <div className="absolute bottom-8 right-6 h-12 flex items-center gap-2 max-w-64 rounded-full border-2 border-zinc-500 bg-black/50 p-1 pl-5">
+          <span className="text-sm truncate">Globee Shirt</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-indigo-600 px-4 font-semibold whitespace-nowrap">
+            R$ 100,00
+          </span>
+        </div>
+      </Link>
+      <Link
+        href={'#'}
+        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-center"
+      >
+        <Image
+          className="scale-95 group-hover:scale-100 transition-transform duration-200"
+          src={'/images/shirt-3.png'}
+          width={300}
+          height={300}
+          quality={100}
+          alt=""
+        />
+
+        <div className="absolute bottom-8 right-6 h-12 flex items-center gap-2 max-w-64 rounded-full border-2 border-zinc-500 bg-black/50 p-1 pl-5">
+          <span className="text-sm truncate">Ernie Ball Shirt</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-indigo-600 px-4 font-semibold whitespace-nowrap">
+            R$ 120,00
+          </span>
+        </div>
+      </Link>
+    </div>
+  )
 }
--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -5,7 +5,7 @@ export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
     <div className="mx-auto grid grid-rows-app_layout min-h-screen max-w-screen-2xl gap-4 p-6 ">
       <Header />
-      <article className="bg-gray-600">{children}</article>
+      <main>{children}</main>
     </div>
   )
 }
