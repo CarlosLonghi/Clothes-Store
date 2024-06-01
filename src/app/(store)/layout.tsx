364d8fc222674f4e798398082d6839517d18bb31
CarlosLonghi <carloslonghi.cl@gmail.com>
2024-06-01 17:58:06 -0300

    feat: add cart context

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,11 +1,14 @@
 import { ReactNode } from 'react'
 import { Header } from '@/components/header'
+import { CartProvider } from '@/contexts/cart-context'
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="mx-auto grid grid-rows-app_layout min-h-screen max-w-screen-2xl gap-4 p-6 ">
-      <Header />
-      <main>{children}</main>
-    </div>
+    <CartProvider>
+      <div className="mx-auto grid grid-rows-app_layout min-h-screen max-w-screen-2xl gap-4 p-6 ">
+        <Header />
+        <main>{children}</main>
+      </div>
+    </CartProvider>
   )
 }
