10f946e7e67bb5c8af1f4c1a7a14ecd07bea492b
CarlosLonghi <carloslonghi.cl@gmail.com>
2024-06-20 22:20:02 -0300

    feat: add loading page to search

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,7 +1,21 @@
+import { Skeleton } from '@/components/skeleton'
+import { LoaderCircleIcon } from 'lucide-react'
+
 export default function LoadingSearch() {
   return (
-    <>
-      <h2>Carregando Busca...</h2>
-    </>
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center gap-2">
+        <p className="text-sm">Buscando produtos</p>
+        <span className="animate-spin">
+          <LoaderCircleIcon strokeWidth={2.5} className="w-5 h-5" />
+        </span>
+      </div>
+
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        <Skeleton className="h-96" />
+        <Skeleton className="h-96" />
+        <Skeleton className="h-96" />
+      </div>
+    </div>
   )
 }
