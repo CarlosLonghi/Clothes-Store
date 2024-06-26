dc4aa2aaa9efc0aa86d76d2b2ef43a08d2d1687f
CarlosLonghi <carlos.longhi@digitalone.com.br>
2024-06-26 14:58:25 -0300

    fix: add Suspense to SearchForm in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { CartWidget } from './cart-widget'
 import { SearchForm } from './search-form'
+import { Suspense } from 'react'
 
 export function Header() {
   return (
@@ -11,7 +12,9 @@ export function Header() {
           <h1 className="text-2xl text-zinc-100 font-bold">Clothes Store</h1>
         </Link>
 
-        <SearchForm />
+        <Suspense fallback={null}>
+          <SearchForm />
+        </Suspense>
       </div>
 
       <div className="flex items-center gap-3">
